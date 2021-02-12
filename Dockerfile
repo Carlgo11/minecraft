eba2de8e43fc6b0feca326ace82102e09d6418a9
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-02-13 00:36:14 +0100

    Clean up dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM alpine
-RUN apk add --no-cache openjdk8-jre-base curl jq
+RUN apk --force-refresh --no-cache --purge -f -u add openjdk11-jre-headless curl jq
 RUN adduser minecraft -u 1000 -D -H
 COPY entrypoint.sh /entrypoint.sh
+USER minecraft
 WORKDIR /minecraft
 CMD /entrypoint.sh
 EXPOSE 25565
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,4 @@ echo "Downloading PaperMC..."
 version=`curl -s "https://papermc.io/api/v1/paper" | jq -r '.versions[0]'`
 curl -o papermc.jar "https://papermc.io/api/v1/paper/${version}/latest/download"
 fi
-echo "Switching to Minecraft user..."
-chown 1000:1000 -R /minecraft
-su minecraft -c "java -jar papermc.jar"
+java -jar papermc.jar
