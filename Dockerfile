e13bfd9ee625549b31d6a8576e7ab1240ca0da3c
Carlgo11 <carlgo11@users.noreply.github.com>
2021-11-20 03:57:39 +0100

    Fix entrypoint permissions & update papermc API

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM alpine
-RUN apk --force-refresh --no-cache --purge -f -u add openjdk11-jre curl jq
+FROM openjdk:17-alpine
+RUN apk --force-refresh --no-cache --purge -f -u add curl jq
 RUN adduser minecraft -u 1000 -D -H
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+RUN mkdir /minecraft; chown 1000 /minecraft -R
 USER minecraft
 WORKDIR /minecraft
+RUN ls -la
 ENTRYPOINT /entrypoint.sh
 EXPOSE 25565
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 umask 0077
-echo "eula=true" > eula.txt;
-if [ ! -f papermc.jar ]; then
-echo "Downloading PaperMC..."
-curl -o papermc.jar "https://papermc.io/api/v1/paper/$(curl -s "https://papermc.io/api/v1/paper" | jq -r '.versions[0]')/latest/download"
+
+[ -z "${MC_VERSION}" ] && version=$(curl -s "https://papermc.io/api/v2/projects/paper/" | jq -r '.versions[-1]') || version="${MC_VERSION}"
+build=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/" | jq -r '.builds[-1]')
+download=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}" | jq -r '.downloads.application.name')
+sha=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}" | jq -r '.downloads.application.sha256')
+
+# Check if papermc.jar is present and hashes match
+if ([ ! -f "papermc.jar" ] || [ "$(cat sha.txt)" != "${sha}" ]); then
+  echo "Downloading PaperMC..."
+  curl -o "papermc.jar" "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/${download}"
+
+  # Opt out of privacy issues
+  mkdir -p plugins/PluginMetrics plugins/bStats
+  echo "opt-out: true" > "plugins/PluginMetrics/config.yml"
+  echo "enabled: false" > "plugins/bStats/config.yml"
+
+  # Create necessary files
+  echo "${sha}" > "sha.txt"
+  echo "eula=true" > "eula.txt"
 fi
+
 java -jar papermc.jar "$@"
