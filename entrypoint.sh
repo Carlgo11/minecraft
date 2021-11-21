e4024112c4400c932b716be7cda84300a05ea0d2
Carlgo11 <carlgo11@users.noreply.github.com>
2021-11-21 01:04:36 +0100

    Reduce curl calls in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 umask 0077
+url="https://papermc.io/api/v2/projects/paper"
+[ -z "${MC_VERSION}" ] && url="${url}/versions/$(curl -s "$url" | jq -r '.versions[-1]')" || url="${url}/versions/${MC_VERSION}"
+url="${url}/builds/$(curl -s "${url}" | jq -r '.builds[-1]')"
+build=$(curl -s "${url}" | base64)
+sha=$(echo "${build}" | base64 -d | jq -r '.downloads.application.sha256')
 
-[ -z "${MC_VERSION}" ] && version=$(curl -s "https://papermc.io/api/v2/projects/paper/" | jq -r '.versions[-1]') || version="${MC_VERSION}"
-build=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/" | jq -r '.builds[-1]')
-download=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}" | jq -r '.downloads.application.name')
-sha=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}" | jq -r '.downloads.application.sha256')
-
-# Check if papermc.jar is present and hashes match
+# Check if papermc.jar is present & hashes match
 if ([ ! -f "papermc.jar" ] || [ "$(cat sha.txt)" != "${sha}" ]); then
   echo "Downloading PaperMC..."
-  curl -o "papermc.jar" "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/${download}"
+  curl -o "papermc.jar" "${url}/downloads/$(echo "${build}" | base64 -d | jq -r '.downloads.application.name')"
 
   # Opt out of privacy issues
   mkdir -p plugins/PluginMetrics plugins/bStats
-  echo "opt-out: true" > "plugins/PluginMetrics/config.yml"
-  echo "enabled: false" > "plugins/bStats/config.yml"
+  echo "opt-out: true" >"plugins/PluginMetrics/config.yml"
+  echo "enabled: false" >"plugins/bStats/config.yml"
 
   # Create necessary files
-  echo "${sha}" > "sha.txt"
-  echo "eula=true" > "eula.txt"
+  echo "${sha}" >"sha.txt"
+  echo "eula=true" >"eula.txt"
 fi
